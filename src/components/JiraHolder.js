6f6fb9e98104d50636ddc1fbda6000939e001e1d
Anu Shibin Joseph Raj <ajosephr@opentext.com>
2021-06-01 12:30:17 +0530

    All core funtionalities working fine

--- src/components/JiraHolder.js
+++ src/components/JiraHolder.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import Constants from '../utils/Constants';
-import JiraInputRow from './JiraInputRow';
 
 const JiraHolder = () => {
 
@@ -62,38 +61,97 @@ const JiraHolder = () => {
         return aJira;
     }
 
-    const rowChanged = () => {
-        console.log('jasTrace: row changed');
+    const getResultLine = (jira) => {
+        console.log('sending results');
+        if(jira.summary != "") {
+        return (
+            <p key={jira.rowid}>- { jira.summary } / priority: "{jira.priority}" assignee: "{jira.assignee}" fixversion: "{jira.fixversion}" cfield: "DevPriority:{jira.devpriority}"</p>
+        )
+        }
     }
 
-    return (
-        <div>
-            <button onClick={addRow}>Add JIRA</button>
-            <button onClick={resetRows}>Reset</button>
-            <table id="jiraholdertable" className="table table-striped">
-                <thead>
-                    <tr>
-                        <th>Jira Summary</th>
-                        <th>Jira Priority</th>
-                        <th>Assignee</th>
-                        <th>Fix Version</th>
-                        <th>Dev Priority</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        state.jiras.map(
-                            (jira) =>
-                                <tr key={jira.rowid}>
-                                    <JiraInputRow rowid={jira.rowid} jira={jira} />
-                                    <td><button onClick={function () { deleteRow(jira.rowid) }}>Delete Row {jira.rowid}</button></td>
-                                </tr>
-                        )
-                    }
-                </tbody>
-            </table>
-        </div>
-    )
+const handleInputChanged = (event) => {
+    // get the name and value of the field that was updated
+    const { name, value } = event.target;
+
+    // rowid is stored as "id" in the <tr> element which has the input field
+    const rowid = parseInt(event.target.parentNode.parentNode.id);
+
+    // update the value by matching the rowid
+    var tempJiraStateHolder = state.jiras;
+    for (var i = 0; i < tempJiraStateHolder.length; i++) {
+        var aJira = tempJiraStateHolder[i];
+        if (aJira.rowid === rowid) {
+            aJira[name] = value;
+        }
+    }
+
+    // update the state
+    setState({
+        jiras: tempJiraStateHolder,
+        lastRowId: state.lastRowId
+    });
+}
+
+return (
+    <div>
+        <button onClick={addRow}>Add JIRA</button>
+        <button onClick={resetRows}>Reset</button>
+        <table id="jiraholdertable" className="table table-striped">
+            <thead>
+                <tr>
+                    <th>Jira Summary</th>
+                    <th>Jira Priority</th>
+                    <th>Assignee</th>
+                    <th>Fix Version</th>
+                    <th>Dev Priority</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    state.jiras.map(
+                        (jira) =>
+                            <tr key={jira.rowid} id={jira.rowid}>
+                                <td>RowId = {jira.rowid}<input name="summary" value={jira.summary} onChange={handleInputChanged}></input></td>
+                                <td>
+                                    <select name="priority" value={jira.priority} onChange={handleInputChanged}>
+                                        <option value="@inherit">Inherit</option>
+                                        <option value="blocker">Blocker</option>
+                                        <option value="critical">Critical</option>
+                                        <option value="major">Major</option>
+                                        <option value="minor">Minor</option>
+                                        <option value="trivial">Trivial</option>
+                                    </select>
+                                </td>
+                                <td>
+                                    <select name="assignee" value={jira.assignee} onChange={handleInputChanged}>
+                                        <option value="@current">Me</option>
+                                        <option value="@inherit">Inherit</option>
+                                    </select>
+                                </td>
+                                <td>
+                                    <select name="fixversion" value={jira.fixversion} onChange={handleInputChanged}>
+                                        <option value="@inherit">Inherit</option>
+                                    </select>
+                                </td>
+                                <td>
+                                    <select name="devpriority" value={jira.devpriority} onChange={handleInputChanged}>
+                                        <option value="@inherit">Inherit</option>
+                                    </select>
+                                </td>
+                                <td><button onClick={function () { deleteRow(jira.rowid) }}>Delete Row {jira.rowid}</button></td>
+                            </tr>
+                    )
+                }
+            </tbody>
+        </table>
+        {
+            state.jiras.map(
+                (jira) => getResultLine(jira)
+            )
+        }
+    </div>
+)
 }
 
 export default JiraHolder;
