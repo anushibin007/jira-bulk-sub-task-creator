d9434e9aa2b0923284e8d576e977e3244edf8cb0
Anu Shibin <anushibin007@gmail.com>
2021-06-06 21:06:59 +0530

    Updated the NavBar and added an About Me link

---
+++ src/components/About.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
+
+function About() {
+	const [show, setShow] = useState(false);
+
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+
+	return (
+		<React.Fragment>
+			<Button variant="navbar-btn" onClick={handleShow}>
+				About Me
+			</Button>
+
+			<Modal show={show} onHide={handleClose}>
+				<Modal.Header>
+					<Modal.Title>About Me</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					Designed & Developed by <a href="https://github.com/anushibin007/jira-bulk-sub-task-creator">Anu Shibin Joseph Raj</a>
+				</Modal.Body>
+				<Modal.Footer>
+					<Button variant="secondary" onClick={handleClose}>
+						Close
+					</Button>
+				</Modal.Footer>
+			</Modal>
+		</React.Fragment>
+	);
+}
+export default About;
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,26 @@
 import "../css/navbar.css";
 import React from "react";
+import About from "./About";
 
 function Navigation() {
 	return (
-		<nav className="navbar navbar-dark bg-dark">
-			<span className="navbar-brand">Jira Bulk Subtask Creator</span>
-		</nav>
+		<div>
+			<nav className="navbar navbar-expand-lg navbar-light bg-light">
+				<a className="navbar-brand" href="/">
+					Jira Bulk Subtask Creator
+				</a>
+				<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
+				</button>
+				<div className="collapse navbar-collapse" id="navbarNav">
+					<ul className="navbar-nav">
+						<li className="nav-item">
+							<About />
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</div>
 	);
 }
 
