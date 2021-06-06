7d915513e12dfcdada2999c366edcb3dbde283aa
Anu Shibin <anushibin007@gmail.com>
2021-06-06 23:35:58 +0530

    Removed unwanted div for HowItWorks

--- src/components/HowItWorks.js
+++ src/components/HowItWorks.js
@@ -3,26 +3,24 @@ import { Accordion, Card } from "react-bootstrap";
 
 const HowItWorks = () => {
 	return (
-		<div>
-			<Accordion>
-				<Card>
-					<Accordion.Toggle as={Card.Header} eventKey="0">
-						<h5>How it works</h5>
-					</Accordion.Toggle>
-					<Accordion.Collapse eventKey="0">
-						<Card.Body>
-							<ol>
-								<li>Copy the text from the Results above</li>
-								<li>Go to your parent JIRA</li>
-								<li>Click on More → Create multiple sub-tasks</li>
-								<li>Paste the content you copied from this page above</li>
-								<li>Click on "Create Sub-Tasks"</li>
-							</ol>
-						</Card.Body>
-					</Accordion.Collapse>
-				</Card>
-			</Accordion>
-		</div>
+		<Accordion>
+			<Card>
+				<Accordion.Toggle as={Card.Header} eventKey="0">
+					<h5>How it works</h5>
+				</Accordion.Toggle>
+				<Accordion.Collapse eventKey="0">
+					<Card.Body>
+						<ol>
+							<li>Copy the text from the Results above</li>
+							<li>Go to your parent JIRA</li>
+							<li>Click on More → Create multiple sub-tasks</li>
+							<li>Paste the content you copied from this page above</li>
+							<li>Click on "Create Sub-Tasks"</li>
+						</ol>
+					</Card.Body>
+				</Accordion.Collapse>
+			</Card>
+		</Accordion>
 	);
 };
 
