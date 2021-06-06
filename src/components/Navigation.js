db890fd2d549356af196e1e41a8bf188f18117e5
Anu Shibin <anushibin007@gmail.com>
2021-06-06 21:22:26 +0530

    NavBar is now a react-bootstrap component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,23 +1,20 @@
 import "../css/navbar.css";
 import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
 import About from "./About";
 
 function Navigation() {
 	return (
 		<div>
-			<nav className="navbar navbar-expand-lg navbar-light bg-light">
-				<span className="navbar-brand">Jira Bulk Subtask Creator</span>
-				<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-					<span className="navbar-toggler-icon"></span>
-				</button>
-				<div className="collapse navbar-collapse" id="navbarNav">
-					<ul className="navbar-nav">
-						<li className="nav-item">
-							<About />
-						</li>
-					</ul>
-				</div>
-			</nav>
+			<Navbar bg="light" expand="lg">
+				<Navbar.Brand>Jira Bulk Subtask Creator</Navbar.Brand>
+				<Navbar.Toggle aria-controls="jsc-navbar" />
+				<Navbar.Collapse id="jsc-navbar">
+					<Nav className="mr-auto">
+						<About />
+					</Nav>
+				</Navbar.Collapse>
+			</Navbar>
 		</div>
 	);
 }
