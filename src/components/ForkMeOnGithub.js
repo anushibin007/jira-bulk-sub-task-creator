8d4b1f758acc29b450c0ef53c60c6fc26c31b605
Anu Shibin Joseph Raj <ajosephr@opentext.com>
2021-06-07 16:09:46 +0530

    A pulse animation added to the ForkMeOnGithub ribbon so that it pulses on page load

--- src/components/ForkMeOnGithub.js
+++ src/components/ForkMeOnGithub.js
@@ -5,7 +5,7 @@ import "../css/forkmeongithub.css";
 const ForkMeOnGitHub = () => {
 	return (
 		<div>
-			<a className="github-fork-ribbon right-bottom fixed" href="https://github.com/anushibin007/jira-bulk-sub-task-creator" data-ribbon="Fork me on GitHub" title="Fork me on GitHub">
+			<a className="github-fork-ribbon right-bottom fixed animate__animated animate__pulse" href="https://github.com/anushibin007/jira-bulk-sub-task-creator" data-ribbon="Fork me on GitHub" title="Fork me on GitHub">
 				Fork me on GitHub
 			</a>
 		</div>
