bbe9ad76b20ce2d60569ed8d0a8b3fd7de07ef15
Anu Shibin Joseph Raj <ajosephr@opentext.com>
2021-06-08 17:50:32 +0530

    Added a simple visitor counter

--- src/components/About.js
+++ src/components/About.js
@@ -19,7 +19,17 @@ function About() {
 					<Modal.Title>About Me</Modal.Title>
 				</Modal.Header>
 				<Modal.Body>
-					Designed & Developed with <span className="heart animate__animated animate__heartBeat animate__infinite">♥</span> by <a href="https://github.com/anushibin007/jira-bulk-sub-task-creator">Anu Shibin Joseph Raj</a>
+					<div className="d-flex flex-column">
+						<div className="p-2">
+							Designed & Developed with <span className="heart animate__animated animate__heartBeat animate__infinite">♥</span> by <a href="https://github.com/anushibin007/jira-bulk-sub-task-creator">Anu Shibin Joseph Raj</a>
+						</div>
+						<div className="d-flex justify-content-center">
+							{/* This line is to make an AJAX call to the update function. This will just increment the counter in the background */}
+							<img src="https://www.freevisitorcounters.com/en/home/counter/836517/t/0" style={{ display: "none" }}></img>
+							{/* This line shows the actual counter */}
+							<img src="https://www.freevisitorcounters.com/en/counter/render/836517/t/0" border="0" className="counterimg"></img>
+						</div>
+					</div>
 				</Modal.Body>
 				<Modal.Footer>
 					<Button variant="info" onClick={handleClose}>
